a90b93773d382e8cfa806485b688e1816f5cecf4
roconno1 <william_r_oconnor@progressive.com>
2023-12-05 16:29:33 -0500

    Change attribute name back to sub_price

--- models/core/dim_orgs.sql
+++ models/core/dim_orgs.sql
@@ -38,7 +38,7 @@ SELECT
     , created_at
     , num_users as users
     , sub_created_at
-    , sub_plan as subscription_plan
+    , sub_plan
     , sub_price
 FROM orgs
 LEFT JOIN user_count USING (org_id)
